7efe0db000501096c0a9ea72f4f0d21cad27aa52
Yaroshenko Margarita <_lampochka@mail.ru>
2022-05-27 08:26:10 +0300

    Добавила задание D7.2 внесла мелкие исправления

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -6,5 +6,5 @@ from .views import NewsList, NewsDetail, Search
 urlpatterns = [
     path('', NewsList.as_view()),
     path('<int:pk>', NewsDetail.as_view()),
-    path('/search', Search.as_view()),
+    path('/search/', Search.as_view()),
 ]
