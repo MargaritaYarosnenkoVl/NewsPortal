ceeeac5471f5792ba90dd19aba4bd294b2061704
Yaroshenko Margarita <_lampochka@mail.ru>
2022-05-28 11:47:38 +0300

    Добавила задание D7.4

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 # Импортируем созданное нами представление
-from .views import NewsList, NewsDetail, Search, NewsEdit, NewsDelete
+from .views import NewsList, NewsDetail, Search, NewsEdit, NewsDelete, NewsAdd
 
 urlpatterns = [
     path('', NewsList.as_view()),
     path('<int:pk>', NewsDetail.as_view(), name='news_'),
     path('search/', Search.as_view(), name='search'),
-    path('add/', Search.as_view(), name='add'),
+    path('add/', NewsAdd.as_view(), name='add'),
     path('<int:pk>/edit', NewsEdit.as_view(), name='edit'),
     path('<int:pk>/delete', NewsDelete.as_view(), name='delete'),
 ]
